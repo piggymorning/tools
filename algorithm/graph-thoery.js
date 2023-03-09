9d1df5077b6bb294dc06d408523a9fa502ea2152
jingxi <lvjjmouth@sina.com>
2023-03-09 09:19:35 +0800

    fix:图论部分代码添加

--- algorithm/graph-thoery.js
+++ algorithm/graph-thoery.js
@@ -1,118 +1,6 @@
-const { ReadGraph } = require('./readGraph')
-class DenseGraph {
-	constructor(n, direacted) {
-		// n为点数
-		this.n = n;
-		// m为边数
-		this.m = 0;
-		this.direacted = direacted;
-		this.g = []
-		for (let i = 0; i < n; i++) {
-			this.g.push([])
-		}
-	}
-	V() {
-		return this.n
-	}
+const { ReadGraph,Component } = require('./readGraph')
 
-	E() {
-		return this.m
-	}
-	// v,w为两个点
-	addEdge(v, w) {
-		if (v < 0 || v >= this.n) {
-			return
-		}
-		if (w < 0 || w >= this.n) {
-			return
-		}
-		if (this.hasEdge(v, w)) {
-			return
-		}
-		this.g[v][w] = true
-		if (!this.direacted) {
-			this.g[w][v] = true
-		}
-		this.m++
-	}
-	hasEdge(v, w) {
-		if (v < 0 || v >= this.n) {
-			return
-		}
-		if (w < 0 || w >= this.n) {
-			return
-		}
-		return this.g[v][w]
-	}
-
-}
-
-class SparseGraph {
-	constructor(n, direacted) {
-		// n为点数
-		this.n = n;
-		// m为边数
-		this.m = 0;
-		this.direacted = direacted;
-		this.g = []
-		for (let i = 0; i < n; i++) {
-			this.g.push([])
-		}
-	}
-	V() {
-		return n
-	}
-
-	E() {
-		return m
-	}
-	// v,w为两个点
-	addEdge(v, w) {
-		if (v < 0 || v >= this.n) {
-			return
-		}
-		if (w < 0 || w >= this.n) {
-			return
-		}
-		// if (this.hasEdge(v, w)) {
-		// 	return
-		// }
-		this.g[v].push(w)
-		if (!this.direacted && v !== w) {
-			this.g[w].push(v)
-		}
-		this.m++
-	}
-	// 可以看出hasEdge判断复杂度为O(n)级别，如果在addEdge中使用，则addEdge也变为了O(n)级，消耗较大，因此暂不使用
-	// 由此也可看出稀疏图在处理此类问题上的缺陷，判断是否已有边需要通过遍历来实现，消耗较大
-	// hasEdge(v, w) {
-	// 	if (v < 0 || v >= n) {
-	// 		return
-	// 	}
-	// 	if (w < 0 || w >= n) {
-	// 		return
-	// 	}
-	// 	let res = false
-	// 	this.g[v].forEach(e=>{
-	// 		if(e === w){
-	// 			res = true
-	// 		}
-	// 	})
-	// 	return res
-	// }
-	show(){
-		for(let i=0;i<this.n;i++){
-			let res = ''
-			for(let j=0;j<this.g[i].length;j++){
-				res = `${res} ${this.g[i][j]}` 
-			}
-			console.log(`vertex ${i}:${res}`)
-		}
-		console.log('end',)
-	}
-}
-
-class adjIterator {
+class sIterator {
 	constructor(graph, v) {
 		this.graph = graph
 		this.v = v
@@ -158,6 +46,135 @@ class dIterator {
 		return this.index >= this.graph.V()
 	}
 }
+class DenseGraph {
+	constructor(n, direacted) {
+		this.iterator = dIterator
+		// n为点数
+		this.n = n;
+		// m为边数
+		this.m = 0;
+		this.direacted = direacted;
+		this.g = []
+		for (let i = 0; i < n; i++) {
+			this.g.push(new Array(n).fill(0))
+		}
+	}
+	V() {
+		return this.n
+	}
+
+	E() {
+		return this.m
+	}
+	// v,w为两个点
+	addEdge(v, w) {
+		if (v < 0 || v >= this.n) {
+			return
+		}
+		if (w < 0 || w >= this.n) {
+			return
+		}
+		if (this.hasEdge(v, w)) {
+			return
+		}
+		this.g[v][w] = 1
+		if (!this.direacted) {
+			this.g[w][v] = 1
+		}
+		this.m++
+	}
+	hasEdge(v, w) {
+		if (v < 0 || v >= this.n) {
+			return
+		}
+		if (w < 0 || w >= this.n) {
+			return
+		}
+		return this.g[v][w]
+	}
+
+	show(){
+		for(let i=0;i<this.n;i++){
+			let res = ''
+			for(let j=0;j<this.g[i].length;j++){
+				res = `${res} ${this.g[i][j]}`
+			}
+			console.log(`${res}\r`)
+		}
+		console.log('end',)
+	}
+
+}
+
+class SparseGraph {
+	static iterator = sIterator
+	constructor(n, direacted) {
+		this.iterator = sIterator
+		// n为点数
+		this.n = n;
+		// m为边数
+		this.m = 0;
+		this.direacted = direacted;
+		this.g = []
+		for (let i = 0; i < n; i++) {
+			this.g.push([])
+		}
+	}
+	V() {
+		return this.n
+	}
+
+	E() {
+		return this.m
+	}
+	// v,w为两个点
+	addEdge(v, w) {
+		if (v < 0 || v >= this.n) {
+			return
+		}
+		if (w < 0 || w >= this.n) {
+			return
+		}
+		// if (this.hasEdge(v, w)) {
+		// 	return
+		// }
+		this.g[v].push(w)
+		if (!this.direacted && v !== w) {
+			this.g[w].push(v)
+		}
+		this.m++
+	}
+	// 可以看出hasEdge判断复杂度为O(n)级别，如果在addEdge中使用，则addEdge也变为了O(n)级，消耗较大，因此暂不使用
+	// 由此也可看出稀疏图在处理此类问题上的缺陷，判断是否已有边需要通过遍历来实现，消耗较大
+	// hasEdge(v, w) {
+	// 	if (v < 0 || v >= n) {
+	// 		return
+	// 	}
+	// 	if (w < 0 || w >= n) {
+	// 		return
+	// 	}
+	// 	let res = false
+	// 	this.g[v].forEach(e=>{
+	// 		if(e === w){
+	// 			res = true
+	// 		}
+	// 	})
+	// 	return res
+	// }
+	show(){
+		for(let i=0;i<this.n;i++){
+			let res = ''
+			for(let j=0;j<this.g[i].length;j++){
+				res = `${res} ${this.g[i][j]}` 
+			}
+			console.log(`vertex ${i}:${res}`)
+		}
+		console.log('end',)
+	}
+	
+}
+
+
 function ergodic1() {
 	let n = 20;
 	let m = 100;
@@ -169,7 +186,7 @@ function ergodic1() {
 	}
 	// console.log('res is:',JSON.stringify({data:G.g}) )
 	for (let j = 0; j < n; j++) {
-		const adj = new adjIterator(G, j)
+		const adj = new sIterator(G, j)
 		let start = `${j}:`
 		for (let w = adj.begin(); !adj.end(); w = adj.next()) {
 			start += `|${w}`
@@ -203,11 +220,26 @@ function ergodic2() {
 // ergodic1()
 // ergodic2()
 
-function test(){
+function test1(){
 	const filename = 'graph1.txt'
-	const g = new SparseGraph(6,false)
-	const r = new ReadGraph(g,filename)
-	g.show()
+	const g1 = new SparseGraph(6,false)
+	const r1 = new ReadGraph(g1,filename)
+	g1.show()
+	const g2 = new DenseGraph(6,false)
+	const r2 = new ReadGraph(g2,filename)
+	g2.show()
 }
 
-test()
+
+function test2(){
+	const g1 = new SparseGraph(13,false)
+	const r1 = new ReadGraph(g1,'graph1.txt')
+	const component1 = new Component(g1)
+	const g2 = new SparseGraph(7,false)
+	const r2 = new ReadGraph(g2,'graph2.txt')
+	const component2 = new Component(g2)
+	console.log('component1:',component1.ccount)
+	console.log('component2',component2.ccount)
+}
+
+test2()
--- algorithm/readGraph.js
+++ algorithm/readGraph.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 class ReadGraph {
 	constructor(graph, filename) {
 		let list = []
@@ -14,6 +14,43 @@ class ReadGraph {
 	}
 }
 
+class Component {
+	constructor(graph,) {
+		this.g = graph
+		// 先想好需要什么状态量以及需要返回什么值
+		this.visited = []
+		this.ccount = 0
+		this.id = new Array(this.g.V()).fill(-1)
+		// 节点列表是知道的，可以直接遍历。遍历的时候，深度优先遍历。终止点就是要把所有的节点全部遍历到
+		for (let i = 0; i < this.g.V(); i++) {
+			if (!this.visited[i]) {
+				// 从任何一个节点开始遍历，都能走完整个连通图
+				this.dfs(i)
+				this.ccount++
+			}
+		}
+	}
+	dfs(i) {
+		this.visited[i] = true
+		this.id[i] = this.ccount
+		const adj = new this.g.iterator(this.g, i)
+		for (let j = adj.begin(); !adj.end(); j = adj.next()) {
+			if (!this.visited[j]) {
+				this.dfs(j)
+			}
+		}
+	}
+	isConnected(v, w) {
+		if (v < 0 || v >= this.g.V()) {
+			return
+		}
+		if (w < 0 || w >= this.g.V()) {
+			return
+		}
+		return this.id[v] === this.id[w]
+	}
+}
+
 module.exports = {
-	ReadGraph,
+	ReadGraph, Component
 }
